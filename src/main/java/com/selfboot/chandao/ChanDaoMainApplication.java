4bff82fabe547ef8fd3f81c7eaffeff9ab7461f4
Yenuoyi <19689405@qq.com>
2019-03-29 10:03:29 +0800

    我就试试看提交的人

--- src/main/java/com/selfboot/chandao/ChanDaoMainApplication.java
+++ src/main/java/com/selfboot/chandao/ChanDaoMainApplication.java
@@ -14,5 +14,6 @@ public class ChanDaoMainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChanDaoMainApplication.class,args);
+        System.out.println("我就试试看提交");
     }
 }
